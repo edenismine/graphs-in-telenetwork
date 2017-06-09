7462ca26083ff0dd48b9ccd86d03c77d9ed05ecd
Daniel Aragon <daniel.aragon@ciencias.unam.mx>
2017-06-09 18:26:32 -0500

    Translated Client class. Added javadoc for client's attributes. Changed Stations' client lists to hashsets to improve remove and contain operations. Created DuplicateAreaCodeException class.

---
+++ src/fciencias/edatos/network/DuplicateAreaCodeException.java
@@ -0,0 +1,9 @@
+package fciencias.edatos.network;
+
+/**
+ * This exception is raised when a duplicate area code is provided when creating or modifying a Station.
+ *
+ * @author Luis Daniel Aragon Bermudez 416041271
+ */
+public class DuplicateAreaCodeException extends Exception {
+}
---
+++ src/fciencias/edatos/network/Station.java
@@ -0,0 +1,103 @@
+package fciencias.edatos.network;
+
+import java.util.HashSet;
+
+/**
+ * Station class that represents the network's stations.
+ *
+ * @author Luis Daniel Aragon Bermudez.
+ * @author Jorge Cortes Lopez.
+ * @author Kai Ueda Kawasaki.
+ */
+public class Station {
+
+    private static final HashSet<Integer> TAKEN_AREA_CODES = new HashSet<>();
+    /**
+     * The station's name.
+     */
+    private String name;
+    /**
+     * The station's area code.
+     */
+    private int areaCode;
+    /**
+     * The clients connected to the station.
+     */
+    private HashSet<Client> clients;
+
+    /**
+     * Creates an empty station with the provided name and area code (unique).
+     *
+     * @param name     The station's name.
+     * @param areaCode The station's unique area code.
+     */
+    public Station(String name, int areaCode) throws DuplicateAreaCodeException {
+        this.name = name;
+        if (TAKEN_AREA_CODES.add(areaCode)) {
+            this.areaCode = areaCode;
+        } else {
+            throw new DuplicateAreaCodeException();
+        }
+        this.clients = new HashSet<>();
+    }
+
+    /**
+     * Retrieves the station's name.
+     *
+     * @return The name of the station.
+     */
+    public String getStationName() {
+        return name;
+    }
+
+    /**
+     * Changes the station's name with the provided String.
+     *
+     * @param name The station's new name.
+     */
+    public void setStationName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Retrieves the station's unique area code.
+     *
+     * @return The unique area code of the Station.
+     */
+    public int getAreaCode() {
+        return areaCode;
+    }
+
+    /**
+     * Changes the station's area code with the provided identifier.
+     *
+     * @param areaCode The station's new araCode.
+     */
+    public void setAreaCode(int areaCode) throws DuplicateAreaCodeException {
+        if (TAKEN_AREA_CODES.add(areaCode)) {
+            TAKEN_AREA_CODES.remove(areaCode);
+            this.areaCode = areaCode;
+        } else {
+            throw new DuplicateAreaCodeException();
+        }
+    }
+
+    /**
+     * Retrieves the clients connected to the station.
+     *
+     * @return The clients connected to the station.
+     */
+    public HashSet<Client> getClients() {
+        return clients;
+    }
+
+    /**
+     * Adds a client to the station, and returns a boolean to indicate if the station was modified as a result.
+     *
+     * @param client The client that should be added to the station.
+     * @return True if the provided client was indeed a new, false otherwise.
+     */
+    public boolean addClient(Client client) {
+        return clients.add(client);
+    }
+}
