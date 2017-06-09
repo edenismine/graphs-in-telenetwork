2f4e9dc6c11a6fc8b9f8903afa0a4268222cffaa
Daniel Aragon <daniel.aragon@ciencias.unam.mx>
2017-06-09 18:54:40 -0500

    Added more methods to Graph, created Vertex interface.

--- src/fciencias/edatos/util/Graph.java
+++ src/fciencias/edatos/util/Graph.java
@@ -1,13 +1,25 @@
 package fciencias.edatos.util;
 
+import java.util.Hashtable;
+
 /**
  * This interface provides operational specifications for a Graph.
  *
  * @param <T> Type associated to the nodes of the graph.
  * @author Luis Daniel Aragon Bermudez 416041271
  */
-public interface Graph<T>{
+public interface Graph<T> {
     int size();
 
     boolean isEmpty();
+
+    boolean addVertex(Vertex<T> vertex);
+
+    boolean addEdge(Vertex<T> fromV, Vertex<T> toV);
+
+    Vertex<T> getVertex(T VertexKey);
+
+    Hashtable<T, Vertex<T>> getVertices();
+
+    boolean contains(Vertex<T> vertex);
 }
---
+++ src/fciencias/edatos/util/Vertex.java
@@ -0,0 +1,8 @@
+package fciencias.edatos.util;
+
+/**
+ * @param <V> Type parameter of this class.
+ * @author Luis Daniel Aragon Bermudez 416041271
+ */
+public interface Vertex<V> {
+}
